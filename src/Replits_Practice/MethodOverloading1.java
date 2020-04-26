5483e33d027f4b0b625daabfaf694f67e462a607
JMossman14 <judy.mossman@yahoo.com>
2020-04-26 17:37:03 -0400

    add commit

---
+++ src/Replits_Practice/MethodOverloading1.java
@@ -0,0 +1,24 @@
+package Replits_Practice;
+
+public class MethodOverloading1 {
+    public static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static double findMax(double[] arr) {
+        double max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
+
