0d32790ae5ed3a2904b585cf78cef44ebfb515db
JMossman14 <judy.mossman@yahoo.com>
2020-04-28 17:21:26 -0400

    ADD

---
+++ src/Replits_Practice/Methods_Palindrome.java
@@ -0,0 +1,26 @@
+package Replits_Practice;
+/*
+Palindrome is a word, phrase, or sequence that reads the same
+backward as forward, e.g., madam or nurses run.
+
+So method isPalindrome checks that and returns true if check is
+palindrome and false if it is not.
+
+- make your conditions case insensitive.  ex: Civic and civic are
+both palindromes
+- make your conditions space insensitive.  Race car is a palindrome
+even though there is space in between.
+
+Examples:
+
+isPalindrome("Noon") ==> true
+
+isPalindrome("I am not palindrome") ==> false
+
+isPalindrome("wooden") ==> false
+
+isPalindrome("Nurses Run") ==> true
+ */
+public class Methods_Palindrome {
+
+}
